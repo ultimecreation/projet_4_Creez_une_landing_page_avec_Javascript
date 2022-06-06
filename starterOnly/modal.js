0e903997821062576f51b68c59b3c3fbe334bb50
ultimecreation <naserbechata@gmail.com>
2022-06-06 15:35:07 +0200

    enabled modal close on click

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,16 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+const closeBtn = document.querySelector('.close')
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal 
+const closeModal = () => modalbg.style.display = "none";
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener('click',closeModal)
