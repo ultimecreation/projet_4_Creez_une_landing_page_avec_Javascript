d87d6508520b47ad8282a2bc9652d1b3a1941b62
ultimecreation <naserbechata@gmail.com>
2022-06-07 16:14:33 +0200

    added errors display + form reset

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,60 +17,106 @@ function editNav() {
     x.className = "topnav";
   }
 }
-// launch modal form
+
+/**
+ * reset input errors 
+ * and remove red borders around inputs
+ * @return  void
+ */
+const resetFormErrors = () => {
+  // bind formData/error__msg css classes
+  const formDataArray = document.querySelectorAll('.formData')
+  const errorMsgContainers = document.querySelectorAll('.error__msg')
+
+  // remove the red borders
+  formDataArray.forEach(formData => {
+    formData.dataset.errorVisible = false
+    console.log(formData.dataset.errorVisible)
+  })
+
+  // remove the error text
+  errorMsgContainers.forEach(errorMsgContainer => {
+    errorMsgContainer.textContent = ''
+  })
+}
+
+/**
+ * launch modal on btn click
+ * resets at start errors if any 
+ * @return  void
+ */
 function launchModal() {
+  resetFormErrors()
   modalbg.style.display = "block";
 }
 
-// close the modal on click
-const closeModal = () => modalbg.style.display = "none";
 
-// validate user inputs
-const validate = () => {
+/**
+ * close the modal when X is clicked
+ * @return  void
+ */
+const closeModal = () => {
+  modalbg.style.display = "none";
+}
+
+/**
+ * [validate description]
+ *
+ * @param  event  
+ *
+ * @return  boolean   
+ */
+const validate = (event) => {
+  // prevent from button behavior and reset errors if any
+  event.preventDefault()
+  resetFormErrors()
+
   // bind data
+  const form = document.querySelector('form[name="reserve"]')
   const first = document.querySelector('#first')
   const last = document.querySelector('#last')
   const email = document.querySelector('#email')
   const quantity = document.querySelector('#quantity')
-  const location = document.querySelector('input[name="location"]')
+  let location = document.querySelector('input[name="location"]:checked')
   const checkbox1 = document.querySelector('#checkbox1')
-  console.log({
-    first: first.value,
-    last: last.value,
-    email: email.value,
-    quantity: quantity.value,
-    location: location.value,
-    checkbox1: checkbox1.checked,
-  })
-
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  
   // check for errors
   let errors = []
-  if(first.value === '')errors.push({ target: first, msg: "Le prénom est requis" })
-  if(last.value === '')errors.push({ target: last, msg: "Le nom est requis" })
-  if(email.value === '')errors.push({ target: email, msg: "L'email est requis" })
-  if(quantity.value === '')errors.push({ target: quantity, msg: "Le nombre de tournois est requis" })
-  if(location.value === '')errors.push({ target: location, msg: "Un lieu de tournois est requise" })
-  if(checkbox1.value === '')errors.push({ target: checkbox1, msg: "L'acceptation des CGU est requise" })
-
-  if(errors.length > 0 ){
-    errors.forEach(error => {
-      console.log(error)
-    })
-    return false
+  if (first.value.length < 2) errors.push({ target: first, msg: "Le prénom est requis ( 2 charactères minimum)" })
+  if (last.value.length < 2) errors.push({ target: last, msg: "Le nom est requis ( 2 charactères minimum)" })
+  if (email.value === '') errors.push({ target: email, msg: "L'email est requis" })
+  else if (!email.value.toLowerCase().match(emailRegex)) errors.push({ target: email, msg: "L'email n'est pas valide" })
+  if (!parseInt(quantity.value)) errors.push({ target: quantity, msg: "Le nombre de tournois est requis" })
+  if (location === null ){
+    // no location selected, set the location input as default to display the error
+    location = document.querySelector('input[name="location"]')
+    errors.push({ target: location, msg: "Un lieu de tournois est requis" })
   } 
-   console.log('Félicitation NAS')
-    return false
-  
+  if (checkbox1.checked !== true) errors.push({ target: checkbox1, msg: "L'acceptation des CGU est requise" })
+ 
+  // some errors found, dispaly them to the user
+  if (errors.length > 0) {
+    errors.map(error => {
 
-}
+      // add the red borders around current input
+      error.target.parentElement.dataset.errorVisible = true
 
-const displayErrors = (errors) => {
- errors.forEach( error => {
-      console.log(error)
+      // get the error container and insert the error msg
+      const currentErrorContainer = document.querySelector(`#${error.target.name}Error`)
+      currentErrorContainer.textContent = `${error.msg}`
+     
     })
+    return false
+  }
+  form.reset()
+  console.log('Félicitation NAS')
+  return false
+
+
 }
 
 // EVENT LISTENENERS
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener('click',closeModal)
+closeBtn.addEventListener('click', closeModal)
